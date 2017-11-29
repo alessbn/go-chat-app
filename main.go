325f215ed90c765c608a10465043b8dbe90168b0
Alejandra Bustos <alejandrabustosn@gmail.com>
2017-11-29 16:23:03 -0600

    Made templ a proper htpp.Handler

--- main.go
+++ main.go
@@ -8,24 +8,24 @@ import (
 )
 
 // templ represents a single template
-type templ struct {
-	source string
-	templ  *template.Template
+type templateHandler struct {
+	filename string
+	templ    *template.Template
 }
 
-// Handle is a http.HandleFunc that renders this template
+// ServeHTTP method handles the HTTP request (load source file, compile the template and execute it)
 // and writes the output to the http.ResponseWriter method
-func (t *templ) Handle(w http.ResponseWriter, r *http.Request) {
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if t.templ == nil {
-		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.source)))
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	}
 	t.templ.Execute(w, nil)
 }
 
 func main() {
-	// http.HandleFunc maps the path pattern "/" to the function passed as the second argument
+	// http.Handle maps the path pattern "/" to the function passed as the second argument
 	// when the user hits http://localhost:8080/ the function will be executed
-	http.HandleFunc("/", (&templ{source: "chat.html"}).Handle)
+	http.Handle("/", (&templateHandler{filename: "chat.html"}))
 	// start the web server on port :8080 using the ListenAndServe method
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
